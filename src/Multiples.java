b9c7dd0eb0e176c76730e87c94211ac3dae96023
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-09-12 15:31:06 -0400

    updated README.md and implemented Multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,12 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
